7064d43cbb31e47f077ed0f7afddb2fe6d7feb56
Amanda Dolci <amanda.dolci@gmail.com>
2024-03-12 19:59:06 -0300

    refactor: change file name & update console return

--- index.js
+++
@@ -1,4 +0,0 @@
-// - Criar um arquivo que dado o array de valores[2, 4, 3, 1, 6, 7, 9],
-//   retorne quais são pares.
-const evenNumbers = array => array.filter(element => element % 2 === 0);
-console.log(evenNumbers([2, 4, 3, 1, 6, 7, 9]));
