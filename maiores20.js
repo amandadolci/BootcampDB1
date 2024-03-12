b91a9dad4e680a28fa6aeed25fe5f44c944d1014
Amanda Dolci <amanda.dolci@gmail.com>
2024-03-12 20:51:51 -0300

    feat: implement lower than 20 on the console

--- maiores20.js
+++ maiores20.js
@@ -1,5 +1,7 @@
 // - Criar um novo arquivo que implemente a solução para o problema: dado um array de valores
 // [9, 4, 5, 10, 2, 90, 8, 6, 33] exiba quais são os maiores que 20.
-const array = [9, 4, 5, 10, 2, 90, 8, 6, 33];
+const array = [9, 4, 5, 10, 2, 90, 8, 6, 33, 20, 8, 10, 4, 7];
 const biggerThan20 = array.filter(element => element > 20);
+const lowerThan20 = array.filter(element => element <= 20);
 console.log(`Os números maiores que 20 são: ${biggerThan20.toString()}.`);
+console.log(`Os números menores ou igual 20 são: ${lowerThan20.toString()}.`);
