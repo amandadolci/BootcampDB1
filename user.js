751dc851a8a2da3e8f9600113d626e8a669dc3dd
amandadolci <118022262+amandadolci@users.noreply.github.com>
2024-03-12 22:10:58 -0300

    feat: create user file (#1)

---
+++ user.js
@@ -0,0 +1,8 @@
+const user = {
+	name: 'Amanda',
+	city: 'Campo Mourão',
+	work: 'desenvolvedora',
+	hobby: 'assistir filmes',
+};
+
+console.log(`${user.name}, de ${user.city}, é ${user.work} e gosta de ${user.hobby}`);
