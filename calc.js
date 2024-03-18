338695b1db61e9f0734fb4a307e95f28d0fa195f
Amanda Dolci <amanda.dolci@gmail.com>
2024-03-18 20:58:39 -0300

    feat: implement math operations file

---
+++ calc.js
@@ -0,0 +1,11 @@
+// - Crie um novo arquivo "calc.js" e implemente soma, subtração, divisão e multiplicação
+// para 2 números(deixar os números fixos no código mesmo).Imprimir os resultados
+const soma = (a, b) => a + b;
+const subtracao = (a, b) => a - b;
+const divisao = (a, b) => a / b;
+const multiplicacao = (a, b) => a * b;
+
+console.log(`O resultado da soma é: ${soma(10, 5)}`);
+console.log(`O resultado da subtração é: ${subtracao(10, 5)}`);
+console.log(`O resultado da divisão é: ${divisao(10, 5)}`);
+console.log(`O resultado da multiplicação é: ${multiplicacao(10, 5)}`);
