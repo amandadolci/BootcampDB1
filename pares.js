b6314b90cf14b7dbe93d1c6a8e0424b3c3af4664
Amanda Dolci <amanda.dolci@gmail.com>
2024-03-18 21:02:09 -0300

    refactor: update array values

--- pares.js
+++ pares.js
@@ -1,5 +1,5 @@
 // - Criar um arquivo que dado o array de valores[2, 4, 3, 1, 6, 7, 9],
 //   retorne quais são pares.
-const array = [2, 4, 3, 1, 6, 7, 9];
+const array = [2, 4, 3, 1, 6, 7, 9, 13, 18, 36, 20];
 const evenNumbers = array.filter(element => element % 2 === 0);
 console.log(`Os números pares são: ${evenNumbers.toString()}.`);
